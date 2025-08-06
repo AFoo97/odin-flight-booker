16af4c740ccda38b26dbc5aa8fc7f5319548f435
AFoo97 <arthur.foozb@gmail.com>
2025-08-06 10:34:10 +1000

    Set up seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,17 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+# Airports
+airport_codes = %w[SFO NYC LAX ORD DFW MIA]
+airport_codes.each { |code| Airport.create!(code: code) }
+
+# Flights
+airports = Airport.all
+10.times do
+  departure = airports.sample
+  arrival = (airports - [departure]).sample
+  Flight.create!(
+    departure_airport: departure,
+    arrival_airport: arrival,
+    start_datetime: Faker::Time.between(from: DateTime.now, to: DateTime.now + 30),
+    duration: rand(90..400)
+  )
+end
+

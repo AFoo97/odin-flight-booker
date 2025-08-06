a256a5ff7cf68930562a399d57424bbdefd88349
AFoo97 <arthur.foozb@gmail.com>
2025-08-06 10:53:32 +1000

    Edited flight controller and seeds to show flights

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -4,11 +4,11 @@ class FlightsController < ApplicationController
     @dates = Flight.pluck(:start_datetime).map { |d| d.to_date }.uniq.sort
     @num_tickets = [1, 2, 3, 4]
 
-    if params[:commit] == "Search"
+    if params[:departure_airport_id].present? && params[:arrival_airport_id].present? && params[:date].present?
       @flights = Flight.where(
         departure_airport_id: params[:departure_airport_id],
         arrival_airport_id: params[:arrival_airport_id]
-      ).where("DATE(start_datetime) = ?", params[:date])
+        ).where(start_datetime: Date.parse(params[:date]).all_day)
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,26 @@
-# Airports
-airport_codes = %w[SFO NYC LAX ORD DFW MIA]
-airport_codes.each { |code| Airport.create!(code: code) }
+# Clear existing data
+Airport.destroy_all
+Flight.destroy_all
 
-# Flights
-airports = Airport.all
-10.times do
-  departure = airports.sample
-  arrival = (airports - [departure]).sample
-  Flight.create!(
-    departure_airport: departure,
-    arrival_airport: arrival,
-    start_datetime: Faker::Time.between(from: DateTime.now, to: DateTime.now + 30),
-    duration: rand(90..400)
-  )
+# Create airports
+airport_codes = %w[SFO NYC LAX ORD DFW MIA]
+airports = airport_codes.map { |code| Airport.create!(code: code) }
+
+# Create flights for today and tomorrow between all airport pairs
+dates = [Date.today, Date.today + 1]
+
+dates.each do |date|
+  airports.each do |departure|
+    (airports - [departure]).each do |arrival|
+      Flight.create!(
+        departure_airport: departure,
+        arrival_airport: arrival,
+        start_datetime: date.to_time.change(hour: rand(6..22)), # random hour
+        duration: rand(90..400)
+      )
+    end
+  end
 end
+
+puts "Seeded #{Airport.count} airports and #{Flight.count} flights."
 
